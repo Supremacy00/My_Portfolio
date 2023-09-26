35e0a8d7b145a17a75ef989a03a9b6b36698f23a
Supremacy00 <musaabdulmuqaddas@gmail.com>
2023-09-26 12:11:14 -0500

    updated

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,3 +1,4 @@
+import { LazyLoadImage } from "react-lazy-load-image-component";
 import { aboutData } from "../../data";
 
 const About = () => {
@@ -14,8 +15,9 @@ const About = () => {
         <p className="text-[17px] mt-4 md:text-start">{aboutText}</p>
       </div>
       <div>
-        <img
+        <LazyLoadImage
           src={aboutImg}
+          loading="lazy"
           alt=""
           className="container mx-auto my-12 max-w-[500px] min-h-[300px] cover md:max-h-[450px] md:max-w-[600px] lg:h-[500px] lg:max-w-[450px] xl:max-w-[530px]"
         />
